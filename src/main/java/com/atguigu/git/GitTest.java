9e502112cb9b6e555b50ccf5df4f17e4eacf2eec
Trip <Trip@atguigu.com>
2023-01-18 16:39:07 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author trip
+ * @create 2023-01-17 22:17
+ */
+public class GitTest {
+    public static void main(String args[]){
+        System.out.println("hello git!");
+    }
+}
