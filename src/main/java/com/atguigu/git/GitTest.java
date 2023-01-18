5afe6c81c71051d6128e17bded6d1d5a2bbadfb9
Trip <Trip@atguigu.com>
2023-01-18 20:09:42 +0800

    master first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,6 +8,6 @@ public class GitTest {
     public static void main(String args[]){
         System.out.println("hello git! hello my friend!");
         System.out.println("hello git");
-        System.out.println("hello git! hot-fix");
+        System.out.println("hello git! hot-fix merge");
     }
 }
