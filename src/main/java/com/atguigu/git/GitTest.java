f2dcfa22e51cad2b9527af7c7cfdb0adb46ac434
Trip <Trip@atguigu.com>
2023-01-19 16:31:24 +0800

    push first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git! master-fix");
         System.out.println("hello git! hot-fix merge");
+        System.out.println("first push!");
     }
 }
